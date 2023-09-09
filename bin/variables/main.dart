7db3aeb35e26c7a91615bb7075c83d82f6453eba
Md Eusuf Uddin <eusuf.uddin@v2.ltd>
2023-09-09 23:07:37 +0600

    Add variable types

---
+++ bin/variables/main.dart
@@ -0,0 +1,31 @@
+import 'dart:ffi';
+
+void main() {
+  /* Non Type Safty variable */
+  var name = "Md Eusuf Uddin";
+  var age = 26;
+  var profession = "Software Developer";
+  var salary = 100.50;
+
+  print("Name: ${name}" +
+      "\n" +
+      "Age: ${age}" +
+      "\n" +
+      "Profession: ${profession}" +
+      "\n" +
+      "Salary${salary}");
+
+/*Type Safty variable */
+  String tName = "Md Shohag Uddin";
+  int tAge = 26;
+  String tProfession = "Pyhon Developer";
+  double tSalary = 50.50;
+
+  print("Name: ${tName}" +
+      "\n" +
+      "Age: ${tAge}" +
+      "\n" +
+      "Profession: ${tProfession}" +
+      "\n" +
+      "Salary${salary}");
+}

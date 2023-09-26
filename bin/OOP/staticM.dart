b3f31f722f991432ae839697cb9d8b0f9670112f
Md Eusuf Uddin <eusuf.uddin@v2.ltd>
2023-09-27 00:24:38 +0600

    static keyword add

---
+++ bin/OOP/staticM.dart
@@ -0,0 +1,26 @@
+void main() {
+  Student std1 = new Student(); // Creating instances of student class
+  Student std2 = new Student();
+  // Assigning value of static variable using class name
+  Student.stdBranch = "Computer Science";
+
+  std1.stdName = "Ben Cutting";
+  std1.roll_num = 90013;
+  std1.showStdInfo();
+
+  std2.stdName = "Peter Handscomb";
+  std2.roll_num = 90014;
+  std2.showStdInfo();
+}
+
+class Student {
+  static late String stdBranch; // Declaring static variable
+  late String stdName;
+  late int roll_num;
+
+  void showStdInfo() {
+    print("Student's name is: ${stdName}");
+    print("Student's salary is: ${roll_num}");
+    print("Student's branch name is: ${stdBranch}");
+  }
+}

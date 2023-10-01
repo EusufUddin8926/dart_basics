f9a6463c303aabdf4338c693c9878403882d759e
EusufUddin8926 <eusuf15-8926@diu.edu.bd>
2023-10-01 13:38:21 +0600

    super constructor add

---
+++ bin/OOP/super_constructor.dart
@@ -0,0 +1,20 @@
+void main() {
+  var testObj = new Child("Ayat Ahmed");
+  testObj.display();
+}
+
+class Parent {
+  Parent(String name) {
+    print("This is from parent class ${name}");
+  }
+}
+
+class Child extends Parent {
+  Child(String name) : super("Eusuf Uddin") {
+    print("This is from Child class ${name}");
+  }
+
+  display() {
+    print("this is default method");
+  }
+}

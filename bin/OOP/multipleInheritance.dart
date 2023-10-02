574033c659fdcf5c039fe24cb1d2784c07c5820a
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-10-02 23:11:03 +0600

    Multiple Inheritance support

---
+++ bin/OOP/multipleInheritance.dart
@@ -0,0 +1,42 @@
+void main() {
+  var departmentsObj = new Department();
+
+  departmentsObj.scienceSubjectName = "Physics";
+  departmentsObj.artssSubjectName = "Histry of Earth";
+
+  departmentsObj.displaySubjectNameScience();
+  departmentsObj.displaySubjectNameHumanitics();
+}
+
+class Science {
+  late String scienceSubjectName;
+
+  void displaySubjectNameScience() {
+    print("The science subject name is ${scienceSubjectName}");
+  }
+}
+
+class Humantics {
+  late String artssSubjectName;
+  void displaySubjectNameHumanitics() {
+    print("The Humantics subject name is ${artssSubjectName}");
+  }
+}
+
+class Department implements Science, Humantics {
+  @override
+  late String artssSubjectName;
+
+  @override
+  late String scienceSubjectName;
+
+  @override
+  void displaySubjectNameHumanitics() {
+    print("My subject name is ${scienceSubjectName}");
+  }
+
+  @override
+  void displaySubjectNameScience() {
+    print("Imrul subject name is ${artssSubjectName}");
+  }
+}

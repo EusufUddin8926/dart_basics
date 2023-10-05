a8fefe992c412713b9a37081ac6b2b6acae9b26d
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-10-05 14:33:46 +0600

    library add

---
+++ bin/library/main.dart
@@ -0,0 +1,10 @@
+import 'calculator.dart';
+
+void main() {
+  var num1 = 20;
+  var num2 = 30;
+
+  print("The number of sum is : ${add(num1, num2)}");
+  print("The modulus of two number is ${modulus(num2, num1)}");
+  print("The random number is ${random(10)}");
+}
